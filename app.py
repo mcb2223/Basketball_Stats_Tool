57a9a9345eecc9bdba71b126e4e420f6e0d51c5f
mcb2223 <105885454+mcb2223@users.noreply.github.com>
2022-06-01 14:53:58 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from ntpath import join
 from operator import index
+from os import name
 from re import A
+
+from numpy import average
 import constants
 from copy import deepcopy
 import sys
@@ -16,16 +19,38 @@ spacer = "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
 exit_message = "Exiting Application. Thank you for visiting!"
 experienced=[]
 not_experienced=[]
-panthers=[]
-bandits=[]
-warriors=[]
+bandits = {
+    "players": [],
+    "experienced": 0,
+    "inexperienced": 0,
+    "player_heights": [],
+    "average_height": None,
+    "guardians":[]
+}
 
+panthers = {
+    "players": [],
+    "experienced": 0,
+    "inexperienced": 0,
+    "player_heights": [],
+    "average_height": None,
+    "guardians": []
+}
+
+warriors = {
+    "players": [],
+    "experienced": 0,
+    "inexperienced": 0,
+    "player_heights": [],
+    "average_height": None,
+    "guardians": []
+}
 
 def clean_data():
     for player in players:
         height= player["height"].split()
         player["height"]=int(height[0])
-        player["guardians"].replace(" and ", ", ")
+        player["guardians"]=player["guardians"].split(" and ")
         if player["experience"] == "YES":
             player["experience"] = True
             experienced.append(player)
@@ -35,35 +60,67 @@ def clean_data():
     return not_experienced, experienced
 
 
-
-
 def team_balancer():
+    
     exp_per_team= int(len(experienced))/len(teams)
     not_exp_per_team= int(len(not_experienced))/len(teams)
     tot_per_team= exp_per_team+not_exp_per_team
 
     for player in experienced:
-        if len (bandits)<exp_per_team:
-            bandits.append(player)
-        elif len(panthers) < exp_per_team:
-            panthers.append(player)
-        elif len(warriors) < exp_per_team:
-            warriors.append(player)
+        if len (bandits["players"])<exp_per_team:
+            bandits["players"].append(player["name"].split("'"))
+            bandits["experienced"] +=1
+            bandits["player_heights"].append(player["height"])
+            bandits["guardians"].append(player["guardians"])
+        elif len(panthers["players"]) < exp_per_team:
+            panthers["players"].append(player["name"].split("'"))
+            panthers["experienced"] += 1
+            panthers["player_heights"].append(player["height"])
+            panthers["guardians"].append(player["guardians"])
+        elif len(warriors["players"]) < exp_per_team:
+            warriors["players"].append(player["name"].split("'"))
+            warriors["experienced"] += 1
+            warriors["player_heights"].append(player["height"])
+            warriors["guardians"].append(player["guardians"])
     for player in not_experienced:
-        if len(bandits) < tot_per_team:
-            bandits.append(player)
-        elif len(panthers) < tot_per_team:
-            panthers.append(player)
-        elif len(warriors) < tot_per_team:
-            warriors.append(player)
+        if len(bandits["players"]) < tot_per_team:
+            bandits["players"].append(player["name"].split(","))
+            bandits["inexperienced"] += 1
+            bandits["player_heights"].append(player["height"])
+            bandits["guardians"].append(player["guardians"])
+        elif len(panthers["players"]) < tot_per_team:
+            panthers["players"].append(player["name"].split(","))
+            panthers["inexperienced"] += 1
+            panthers["player_heights"].append(player["height"])
+            panthers["guardians"].append(player["guardians"])
+        elif len(warriors["players"]) < tot_per_team:
+            warriors["players"].append(player["name"].split(","))
+            warriors["inexperienced"] += 1
+            warriors["player_heights"].append(player["height"])
+            warriors["guardians"].append(player["guardians"])
+            
+    team_cleaner(panthers)
+    team_cleaner(bandits)
+    team_cleaner(warriors)
+
+    
+
+
+#additional stuff here to order players by height
+    #start empty list
+    #loop throught original list
+    #find shortest
+    #variable that contains lowest height or name or index, then after loop pop him into ordered list.    
+
     
     return bandits, panthers, warriors
     
-    
-        
+
 
 
 def console_menu():
+    
+    
     team_1= "Panthers"
     team_2= "Bandits"
     team_3= "Warriors"
@@ -132,24 +189,26 @@ Enter the letter of which team you would like to analyze """)
         return console_menu()
 
 
+def team_cleaner(team):
+    team["players"] = ", ".join([", ".join(x) for x in team["players"]])
+    team["guardians"] = ", ".join([", ".join(x) for x in team["guardians"]])
+    team["average_height"]=sum(bandits["player_heights"])/len(team["player_heights"]) 
+
 def player_stats(team):
-    names= [player["name"] for player in team]
-    guardians=[]
-    height = [player["height"] for player in team]
-    for player in team:
-        guardian=player["guardians"]
-        guardians.append(str(guardian))
-    height_avg= round(sum(height)/len(team),1)
+    players= team["players"]
+    guardians= team["guardians"]
     print(f"""
-        Total players: {int(len(constants.PLAYERS) / len(constants.TEAMS))}
-        Total experienced: {int(len(experienced) / len(teams))}
-        Total inexperienced: {int(len(not_experienced)/ len(teams))}
-        Average height: {float(height_avg)}
+        Total players: {team["experienced"]+team["inexperienced"]}
+        Total experienced: {team["experienced"]}
+        Total inexperienced: {team["inexperienced"]}
     """)
     print("Player names: ")
-    print( "     "+", ".join(names))
+    print(f"      {players}")
     print("Guardian Names: ")  
-    print( "     "+", ".join(guardians))
+    print(f"      {guardians}")
+    
+    
+
 
 
 def other_teams():
